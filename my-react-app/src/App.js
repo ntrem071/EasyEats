2945dcfa88e8a41cebeae5d07e5568eb7d6f7f29
Natasha Tremblay Daszkiewicz <90850975+ntrem071@users.noreply.github.com>
2024-07-07 18:39:22 -0400

    Initial commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import { Navbar } from './Navbar';
+import { Home, Plans, Recipes, Faq, Meal } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element = {<Home />}/>
+        <Route path="/plan" element = {<Plans />}/>
+        <Route path="/recipe" element = {<Recipes />}/>
+        <Route path="/faq" element = {<Faq />}/>
+        <Route path="/meal/:id" element={<Meal />} />
+      </Routes>
     </div>
   );
 }
